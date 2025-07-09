6cc019f5e710147bce2a40901a4afe870ce41540
8KOHAN <kohanzom@gmail.com>
2025-07-09 12:51:08 +0300

    Modifying a template

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from backend.app.routers import characters
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+app.mount("/static", StaticFiles(directory="backend/app/static"), name="static")
+templates = Jinja2Templates(directory="backend/app/templates")
+
+app.include_router(characters.router)
